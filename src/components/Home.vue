e9dfc743a03ae7cdd29ea3fa95d461ebabbbc31e
fimnight <zominwgy@gmail.com>
2025-07-05 17:40:50 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<script>
+import MyComponent from './MyComponent.vue'; // Path จะเป็น './MyComponent.vue' เพราะอยู่โฟลเดอร์เดียวกัน
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  components: {
+    MyComponent // ลงทะเบียน
+  }
+}
+</script>
+
+<template>
+  <div class="hello">
+    <MyComponent /> </div>
+</template>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>สวัสดีจาก MyComponent!</h2>
+    <p>นี่คือข้อความจากคอมโพเนนต์แยก.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent' // ชื่อของคอมโพเนนต์นี้
+}
+</script>
+
+<style scoped>
+h2 {
+  color: rgb(160, 83, 57);
+  text-align: center; /* เพิ่มให้ข้อความอยู่ตรงกลาง */
+}
+p {
+  text-align: center; /* เพิ่มให้ข้อความอยู่ตรงกลาง */
+}
+</style>
